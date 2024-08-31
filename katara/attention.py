db61e5d970a6441321f2e8b508008780d5eb25f7
Tensor <narutokelechi@gmail.com>
2024-08-31 06:35:46 +0100

    [modify] add VAE attention block, finish with cross-attention module

--- katara/attention.py
+++ katara/attention.py
@@ -15,8 +15,6 @@ class SelfAttention(nn.Module):
         self.out_project = nn.Linear(embed_dim, embed_dim, bias=True)
 
     def forward(self, x: torch.Tensor, causal_mask=True):
-        batch_size, seq_len, embed_dim = x.shape
-
         x = self.in_project(x)
 
         # split query, key and value matrices from input tensor
@@ -41,9 +39,11 @@ class SelfAttention(nn.Module):
         attn_weight /= math.sqrt(self.head_dim)
         attn_score = func_nn.softmax(attn_weight, dim=1)
 
-        output = attn_score @ v  # multiply with value matrix
-        # shape -> (batch_size, sequence_length, dimension)
+        output = attn_score @ v  # multiply with value matrixA
+
+        # reshape -> (batch_size, sequence_length, dimension)
         output = rearrange(output, "b h l d -> b l (h d)")
+
         output = self.out_project(output)  # output linear projection
 
         return output
@@ -59,11 +59,7 @@ class CrossAttention(nn.Module):
         self.k_project = nn.Linear(cross_dim, embed_dim, bias=True)
         self.v_project = nn.Linear(cross_dim, embed_dim, bias=True)
 
-    def forward(
-        self, x_input: torch.Tensor, y_target: torch.Tensor, causal_mask: bool = True
-    ):
-        batch_size, seq_len, embed_dim = x_input.shape
-
+    def forward(self, x_input: torch.Tensor, y_target: torch.Tensor):
         q = self.q_project(x_input)  # q -> latent
         k = self.k_project(y_target)  # key -> context, text, mapping
         v = self.v_project(y_target)  # context value matrices
@@ -73,15 +69,36 @@ class CrossAttention(nn.Module):
         k = rearrange(k, "b l (h d) -> b h l d")
         v = rearrange(v, "b l (h d) -> b h l d")
 
-        attn_weight = q @ k.transpose(-1, -2)  # attention matmul
+        attn_weight = q @ k.transpose(-1, -2)  # multiply query / key values
 
         # divide by head dim, d from the attention equation
         attn_weight /= math.sqrt(self.head_dim)
-        attn_score = func_nn.softmax(attn_weight, dim=1)
+        attn_score = func_nn.softmax(attn_weight, dim=1)  # apply spftmax
 
         output = attn_score @ v  # multiply with value matrix
+
         # shape -> (batch_size, sequence_length, dimension)
+        # reshape for output
         output = rearrange(output, "b h l d -> b l (h d)")
-        output = self.out_project(output)  # output linear projection
+        output = self.out_project(output)  # output projection
 
         return output
+
+
+class VaeAttention(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.channels = channels
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+
+    def forward(self, x: torch.Tensor):
+        residue = x
+        bs, ch, h, w = x.shape  # batch_size, channels, height, width
+        x = self.groupnorm(x)
+        x = rearrange(x, "b c h w -> b (h w) c")
+        x = self.attention(x)  # apply self-attention without causal mask
+
+        x = rearrange(x, "b (h w) c -> b c h w")
+
+        return x + residue
