d8c8d8a623c7cacd931c9ce51a68d2e38805a159
Tensor <narutokelechi@gmail.com>
2024-09-01 17:15:17 +0100

    [modify] add CLIP encoding class/layers

--- katara/clip.py
+++ katara/clip.py
@@ -1,26 +1,49 @@
 import torch
 from torch import nn
 from .embeddings import ClipEmbeddings
+from .attention import SelfAttention
 
 
 class ClipLayer(nn.Module):
-    def __init__(self, embed_dim: int = 768):
+    def __init__(self, embed_dim: int = 768, attn_heads: int = 12):
         super().__init__()
         self.layernorm = nn.LayerNorm(embed_dim)
+        self.attention = SelfAttention(attn_heads, embed_dim)
+        self.linear_1 = nn.Linear(embed_dim, 4 * embed_dim)
+        self.linear_2 = nn.Linear(4 * embed_dim, embed_dim)
 
     def forward(self, x: torch.Tensor):
+        residue_a = x
+        x = self.layernorm(x)  # layernorm 1
+        # self attention with causal mask
+        x = self.attention(x, causal_mask=True)
+        x = x + residue_a  # first residual connection
+
+        # feed forward layer
+        residue_b = x
+
         x = self.layernorm(x)
+        x = self.linear_1(x)  # map to higher dim space
+        x = x * torch.sigmoid(1.702 * x)  # QuickGELU activation
+        x = self.linear_2(x)
+        x = x + residue_b  # second residual connection
 
         return x
 
 
 class ClipEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, n_layers: int = 12, embed_dim: int = 768):
         super().__init__()
         self.embedding = ClipEmbeddings()
-        self.layers = ClipLayer()
+        self.norm_layer = nn.LayerNorm(embed_dim)
 
-    def forward(self, x: torch.Tensor):
-        x = self.token_embedding(x)
+        # clip-text encoding layers
+        self.layers = [ClipLayer() for _ in range(n_layers)]
+        self.clip_layers = nn.Sequential(*self.layers)
 
-        return x + self.pos_embed
+    def forward(self, x_token: torch.LongTensor) -> torch.Tensor:
+        x_embed = self.embedding(x_token)
+        x = self.clip_layers(x_embed)
+        x = self.norm_layer(x)
+
+        return x
